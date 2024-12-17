9d8693c86592798b25306565c01310c70f058cbc
Mark Dawson <mark@markdawson.io>
2024-12-17 11:43:08 -0800

    register cron expr with task

--- app/app.ts
+++ app/app.ts
@@ -46,7 +46,7 @@ const taskConfig: HyrexTaskConfigInput = {
         name: "serial-queue",
         concurrencyLimit: 2
     }),
-    cron: "* * * * *"
+    // cron: "* * * * *"
 }
 
 const restartDatabaseTask = hy.task(restartDatabase, taskConfig)

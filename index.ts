e212af9a0c49a3294e8620cd59a2c07670d026e8
Mark Dawson <mark@markdawson.io>
2024-12-21 20:05:49 -0600

    added typedoc

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { HyrexWorker } from "./HyrexWorker";
-import { HyrexRegistry } from "./HyrexRegistry";
+// import { HyrexWorker } from "./HyrexWorker";
+// import { HyrexRegistry } from "./HyrexRegistry";
 
-export { HyrexWorker, HyrexRegistry }
-
+export { HyrexWorker} from "./HyrexWorker"
+export { HyrexRegistry } from "./HyrexRegistry"
 export { getHyrexContext } from './HyrexContext';
 export type { HyrexContext } from './HyrexContext';
